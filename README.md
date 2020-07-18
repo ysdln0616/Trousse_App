7b3b6377878c9efb5ee2b9d6172a765a446f5322
ysdln0616 <1419099@ed.tus.ac.jp>
2020-07-18 19:33:45 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Trousse_App
---
+++ Trousse_App/Trousse_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trousse_App
+//
+//  Created by 吉田琉夏 on 2020/07/18.
+//  Copyright © 2020 吉田琉夏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
